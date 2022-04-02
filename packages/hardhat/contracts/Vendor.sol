fd767eb4b7b3a1c2128e7cd422026d49d98bd272
Pelumi527 <fatolupelumi@gmail.com>
2022-04-02 23:00:13 +0100

    challenge-2-done

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,41 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
+
+  uint256 public constant tokensPerEth = 100;
+
+  error NotEnoughETH();
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable returns(bool) {
+    uint amountOfTokens = (msg.value*100);
+    if(msg.value < 0) revert NotEnoughETH();
+    yourToken.transfer(msg.sender, amountOfTokens);
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+    return true;
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner returns(bool){
+    uint EthBalance = address(this).balance;
+    payable(msg.sender).transfer(EthBalance);
+    return true;
+  }
 
   // ToDo: create a sellTokens() function:
+  function sellTokens(uint amountToSell) public returns(bool) {
+    require(amountToSell > 0, "amount to sell is less than 0");
+    yourToken.transferFrom(msg.sender, address(this), amountToSell);
+    uint ethValue = amountToSell/100;
+    payable(msg.sender).transfer(ethValue);
+    return true;
+  }
 
 }
